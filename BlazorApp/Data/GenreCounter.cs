005514258eaff2707d5f6c387cc8ebf77597ea63
dpm4891 <dpm4891@yahoo.com>
2021-08-06 06:55:24 -0500

    Playing around with DI

---
+++ BlazorApp/Data/GenreCounter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class GenreCounter : IGenreCounter
+    {
+        public int SciFiCounter { get; set; }
+        public int FantasyCounter { get; set; }
+
+        public GenreCounter()
+        {
+
+        }
+
+        public void IncrementCounter(string genre, bool approved)
+        {
+            var a = approved ? (genre == "SciFi" ? SciFiCounter++ : FantasyCounter++) : (genre == "SciFi" ? SciFiCounter-- : FantasyCounter--);
+        }
+    }
+}
---
+++ BlazorApp/Data/IGenreCounter.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp.Data
+{
+    public interface IGenreCounter
+    {
+        int FantasyCounter { get; set; }
+        int SciFiCounter { get; set; }
+
+        void IncrementCounter(string genre, bool approved);
+    }
+}
